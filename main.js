ba9397449ba6609f55c4171b72dda433053d636e
LyricWulf <lyricwulf@gmail.com>
2024-04-01 21:26:41 -0700

    feat: add item names for pretty logging :>

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 async function Sleep(milliseconds) {await new Promise((done) => {return setTimeout(done, milliseconds);});}
+const nameMap = JSON.parse('{"1214340999644446720":"Buster Blade","1214340999644446721":"Cute Plushie","1214340999644446722":"Wump Shell","1214340999644446723":"Speed Boost","1214340999644446724":"⮕⬆⬇⮕⬆⬇","1214340999644446725":"Power Helmet","1214340999644446726":"Quack!!","1214340999644446727":"OHHHHH BANANA","1214340999644446728":"Dream Hammer"}');
 const items = {};
 
 async function Main () {
@@ -7,10 +8,11 @@ async function Main () {
 
     const body = await response.json();
 
-    if (body.opened_item in items) {
-      items[body.opened_item]++;
+    const itemName = nameMap[body.opened_item];
+    if (itemName in items) {
+      items[itemName]++;
     } else {
-      items[body.opened_item] = 1;
+      items[itemName] = 1;
     }
 
     console.log(items);
